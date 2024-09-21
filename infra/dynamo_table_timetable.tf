3af32e2618ba2a2a5124a2660cace6af82edeab2
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2024-09-20 23:34:30 -0300

    infra: remove secondary indexes

--- infra/dynamo_table_timetable.tf
+++ infra/dynamo_table_timetable.tf
@@ -21,105 +21,47 @@ resource "aws_dynamodb_table" "timetable-dynamodb-table" {
   #    type = "BOOL"
   #  }
 
-  attribute {
-    name = "sunday"
-    type = "SS"
-  }
-
-  attribute {
-    name = "monday"
-    type = "SS"
-  }
-
-  attribute {
-    name = "tuesday"
-    type = "SS"
-  }
-
-  attribute {
-    name = "wednesday"
-    type = "SS"
-  }
-
-  attribute {
-    name = "thursday"
-    type = "SS"
-  }
-
-  attribute {
-    name = "friday"
-    type = "SS"
-  }
-
-  attribute {
-    name = "saturday"
-    type = "SS"
-  }
+  #  attribute {
+  #    name = "sunday"
+  #    type = "SS"
+  #  }
+  #
+  #  attribute {
+  #    name = "monday"
+  #    type = "SS"
+  #  }
+  #
+  #  attribute {
+  #    name = "tuesday"
+  #    type = "SS"
+  #  }
+  #
+  #  attribute {
+  #    name = "wednesday"
+  #    type = "SS"
+  #  }
+  #
+  #  attribute {
+  #    name = "thursday"
+  #    type = "SS"
+  #  }
+  #
+  #  attribute {
+  #    name = "friday"
+  #    type = "SS"
+  #  }
+  #
+  #  attribute {
+  #    name = "saturday"
+  #    type = "SS"
+  #  }
 
   #  attribute {
   #    name = "createdAt"
   #    type = "S"
   #  }
 
-  global_secondary_index {
-    name               = "TimetableMondayIndex"
-    hash_key           = "doctorId"
-    range_key          = "monday"
-    write_capacity     = 1
-    read_capacity      = 1
-    projection_type    = "INCLUDE"
-    non_key_attributes = ["id"]
-  }
 
-  global_secondary_index {
-    name               = "TimetableTuesdayIndex"
-    hash_key           = "doctorId"
-    range_key          = "tuesday"
-    write_capacity     = 1
-    read_capacity      = 1
-    projection_type    = "INCLUDE"
-    non_key_attributes = ["id"]
-  }
-
-  global_secondary_index {
-    name               = "TimetableWednesdayIndex"
-    hash_key           = "doctorId"
-    range_key          = "wednesday"
-    write_capacity     = 1
-    read_capacity      = 1
-    projection_type    = "INCLUDE"
-    non_key_attributes = ["id"]
-  }
-
-  global_secondary_index {
-    name               = "TimetableThursdayIndex"
-    hash_key           = "doctorId"
-    range_key          = "thursday"
-    write_capacity     = 1
-    read_capacity      = 1
-    projection_type    = "INCLUDE"
-    non_key_attributes = ["id"]
-  }
-
-  global_secondary_index {
-    name               = "TimetableFridayIndex"
-    hash_key           = "doctorId"
-    range_key          = "friday"
-    write_capacity     = 1
-    read_capacity      = 1
-    projection_type    = "INCLUDE"
-    non_key_attributes = ["id"]
-  }
-
-  global_secondary_index {
-    name               = "TimetableSaturdayIndex"
-    hash_key           = "doctorId"
-    range_key          = "friday"
-    write_capacity     = 1
-    read_capacity      = 1
-    projection_type    = "INCLUDE"
-    non_key_attributes = ["id"]
-  }
 
   tags = {
     Name        = "timetable-dynamodb-table"
