78dd280e7a6706d8f52dc54a26d7022894a92693
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2024-09-21 12:43:16 -0300

    infra: cria tabelas de paciente e medico

---
+++ infra/dynamo_table_pacient.tf
@@ -0,0 +1,82 @@
+resource "aws_dynamodb_table" "appointment-dynamodb-table" {
+  name           = "Appointment"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "id"
+  #  range_key      = "email"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+#  attribute {
+#    name = "isActive"
+#    type = "BOOL"
+#  }
+
+  #  attribute {
+  #    name = "name"
+  #    type = "S"
+  #  }
+
+  #  attribute {
+  #    name = "birthday"
+  #    type = "S"
+  #  }
+
+  attribute {
+    name = "cpf"
+    type = "S"
+  }
+
+  attribute {
+    name = "email"
+    type = "S"
+  }
+
+  #  attribute {
+  #    name = "password"
+  #    type = "S"
+  #  }
+
+  #  attribute {
+  #    name = "contactNumber"
+  #    type = "S"
+  #  }
+
+  #  attribute {
+  #    name = "creationTimestamp"
+  #    type = "S"
+  #  }
+
+  #  attribute {
+  #    name = "updateTimestamp"
+  #    type = "S"
+  #  }
+
+
+  global_secondary_index {
+    name               = "EmailIndex"
+    hash_key           = "email"
+    write_capacity     = 1
+    read_capacity      = 1
+    projection_type    = "INCLUDE"
+    non_key_attributes = ["id"]
+  }
+
+  global_secondary_index {
+    name               = "CpfIndex"
+    hash_key           = "cpf"
+    write_capacity     = 1
+    read_capacity      = 1
+    projection_type    = "INCLUDE"
+    non_key_attributes = ["id"]
+  }
+
+  tags = {
+    Name        = "appointment-dynamodb-table"
+    Environment = "fiap-pos-tech"
+  }
+}
