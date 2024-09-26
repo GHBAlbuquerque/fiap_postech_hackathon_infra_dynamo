556f6b60a9a052456570cba9ea978ea471333692
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2024-09-26 18:16:20 -0300

    feature: adiciona campo de status no appointment

--- infra/dynamo_table_appointment.tf
+++ infra/dynamo_table_appointment.tf
@@ -41,7 +41,7 @@ resource "aws_dynamodb_table" "appointment-dynamodb-table" {
     write_capacity     = 1
     read_capacity      = 1
     projection_type    = "INCLUDE"
-    non_key_attributes = ["doctorId", "scheduledDate", "timeslot", "createdAt"]
+    non_key_attributes = ["doctorId", "scheduledDate", "timeslot", "appointmentStatus", "createdAt"]
   }
 
   global_secondary_index {
@@ -50,7 +50,7 @@ resource "aws_dynamodb_table" "appointment-dynamodb-table" {
     write_capacity     = 1
     read_capacity      = 1
     projection_type    = "INCLUDE"
-    non_key_attributes = ["patientId", "scheduledDate", "timeslot", "createdAt"]
+    non_key_attributes = ["patientId", "scheduledDate", "timeslot", "appointmentStatus", "createdAt"]
   }
 
   global_secondary_index {
@@ -60,7 +60,7 @@ resource "aws_dynamodb_table" "appointment-dynamodb-table" {
     write_capacity     = 1
     read_capacity      = 1
     projection_type    = "INCLUDE"
-    non_key_attributes = ["patientId","scheduledDate", "timeslot", "createdAt"]
+    non_key_attributes = ["patientId","scheduledDate", "timeslot", "appointmentStatus", "createdAt"]
   }
 
   tags = {
